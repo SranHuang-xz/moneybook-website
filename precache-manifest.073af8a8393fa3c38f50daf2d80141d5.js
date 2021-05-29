30fc01eae5bb66517f1a7bd4ee6a6dfb39ccf31e
SranHuang_1005 <763913797@qq.com>
2021-05-30 00:24:01 +0800

    deploy

---
+++ precache-manifest.073af8a8393fa3c38f50daf2d80141d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "434c582b3ec3ce0f898e",
+    "url": "/moneybooking-website/css/app.f20cbd19.css"
+  },
+  {
+    "revision": "9e72ebc384a72a0a79cdbbcb9b62a961",
+    "url": "/moneybooking-website/index.html"
+  },
+  {
+    "revision": "434c582b3ec3ce0f898e",
+    "url": "/moneybooking-website/js/app.0c0e4309.js"
+  },
+  {
+    "revision": "6655d5be963b8b1cc94e",
+    "url": "/moneybooking-website/js/chunk-vendors.36e273ea.js"
+  },
+  {
+    "revision": "e06b967acf8b980ba74920b47ea1ddc2",
+    "url": "/moneybooking-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/moneybooking-website/robots.txt"
+  }
+]);
